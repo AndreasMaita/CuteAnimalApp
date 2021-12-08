06dcec12fcfc7f7f71f03da087a30c20f42d93a5
AndreasMaita <andreaspeppinomaita95@gmail.com>
2021-12-08 12:04:02 +0100

    removed comments

--- lib/core/widgets/dog_image.dart
+++ lib/core/widgets/dog_image.dart
@@ -29,18 +29,3 @@ class DogImageWidget extends StatelessWidget {
     );
   }
 }
-
-/**
- * Image.network(imgURL,fit: BoxFit.fill,
-  loadingBuilder:(BuildContext context, Widget child,ImageChunkEvent loadingProgress) {
-  if (loadingProgress == null) return child;
-    return Center(
-      child: CircularProgressIndicator(
-      value: loadingProgress.expectedTotalBytes != null ? 
-             loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes
-             : null,
-      ),
-    );
-  },
-),
- */
